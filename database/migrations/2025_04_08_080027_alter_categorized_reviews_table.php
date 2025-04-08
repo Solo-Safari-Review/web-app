8d0178fa15c4fc6aa233218e78a33f1e45752ade
CahyaW06 <yudhacahyawijaya@gmail.com>
2025-04-08 15:05:54 +0700

    alter categorized_reviews table

---
+++ database/migrations/2025_04_08_080027_alter_categorized_reviews_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('categorized_reviews', function (Blueprint $table) {
+            $table->renameColumn('departement_admin_comment', 'department_admin_comments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('categorized_reviews', function (Blueprint $table) {
+            $table->renameColumn('department_admin_comment', 'departement_admin_comments');
+        });
+    }
+};
