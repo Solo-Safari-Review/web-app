88e42b0ccda48ff8c9cd24ae7058e6d75b456c9a
CahyaW06 <yudhacahyawijaya@gmail.com>
2025-04-10 14:45:20 +0700

    add categorized review show logic

--- app/Http/Controllers/CategorizedReviewController.php
+++ app/Http/Controllers/CategorizedReviewController.php
@@ -123,9 +123,17 @@ class CategorizedReviewController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(CategorizedReview $categorizedReview)
+    public function show(Request $request)
     {
-        //
+        try {
+            $categorizedReviewId = Crypt::decryptString($request->route('categorized_review'));
+        } catch (\Exception $e) {
+            abort(400, 'Invalid review token');
+        }
+
+        $categorizedReview = CategorizedReview::with('review')->find($categorizedReviewId);
+
+        return response()->json($categorizedReview);
     }
 
     /**
