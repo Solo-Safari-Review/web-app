24c955c9ebd22ba7e82324d07196bca30757b866
CahyaW06 <yudhacahyawijaya@gmail.com>
2025-05-01 12:57:22 +0700

    Revert "add action button"
    This reverts commit 4854d6985204a95aa0334dc72dd4f627144c4979.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,12 @@
 @endsection
 
 @section('main')
-<div class="flex flex-wrap gap-8 w-full items-center justify-center">
+<div class="flex flex-wrap gap-8 w-full align-middle justify-center">
     <x-card-category label="Toilet" number="150"></x-card-category>
     <x-card-category label="Keamanan & Kebersihan" number="87"></x-card-category>
     <x-card-category label="Area Parkir" number="25"></x-card-category>
     <x-card-category label="Hewan" number="7"></x-card-category>
     <x-card-category label="Pegawai" number="5"></x-card-category>
-    <x-action-button></x-action-button>
 </div>
 @endsection
 
