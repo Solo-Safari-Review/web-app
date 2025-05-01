ec38d0db8206e34e3a60b3f225bb26cc851fc066
CahyaW06 <yudhacahyawijaya@gmail.com>
2025-05-01 13:07:12 +0700

    migration to tailwind v4

--- resources/views/components/card-category.blade.php
+++ resources/views/components/card-category.blade.php
@@ -1,5 +1,5 @@
 <div class="flex flex-col px-4 py-4 rounded-2xl bg-[#907B60] text-white items-start align-bottom">
-    <span class="flex-grow font-bold text-[20px]">{{ $label }}</span>
+    <span class="grow font-bold text-[20px]">{{ $label }}</span>
     <div class="flex gap-2 items-end align-bottom">
         <span class="font-bold text-[72px]">{{ $number }}</span>
         <span class="text-[16px]">Ulasan</span>
