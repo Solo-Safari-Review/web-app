3b90d4f009b799b11a49f52ea5322fdf44f37d59
CahyaW06 <yudhacahyawijaya@gmail.com>
2025-05-04 02:09:14 +0700

    update review item component

--- resources/views/components/review-item.blade.php
+++ resources/views/components/review-item.blade.php
@@ -1,8 +1,8 @@
-<div class="flex xl:flex-row flex-col gap-8 px-4 py-4 items-center justify-start w-full border-b border-[#535151]/30">
+<div class="flex xl:flex-row flex-col gap-8 px-4 py-4 items-center justify-start w-full border-b border-[#535151]/30 has-checked:bg-[#E9D9C7]/30">
     <div class="flex xl:flex-row flex-col gap-8 items-center justify-start w-full">
         <div class="flex gap-2 items-center justify-between xl:justify-start w-full xl:max-w-[300px]">
             <div class="flex gap-4 items-center w-[200px]">
-                <input type="checkbox" value="" class="w-[18px] h-[18px] me-4 text-[#907B60] bg-gray-100 border-gray-300 focus:ring-[#907B60]">
+                <input type="checkbox" value="{{ $review }}" class="w-[18px] h-[18px] me-4 text-[#907B60] bg-gray-100 border-gray-300 focus:ring-[#907B60]">
                 <span>{{ $username }}</span>
             </div>
             <span>{{ $category }}</span>
